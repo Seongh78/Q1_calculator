44b00ed4c1e13ca40d3cbbaa065145ddd5c8bb31
hyeon92 <ghkgus92@naver.com>
2019-04-25 14:58:42 +0900

    기능추가

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var undoButton = document.getElementById('undoButton'),
   redoButton = document.getElementById('redoButton'),
   inputValue = document.getElementById('inputbox');
 
+var calList = new Array();
+var cnt = -1;
 // =====  ===== //
 function onload() {
   undoButton.onclick = handleClick;
@@ -23,24 +25,74 @@ function onload() {
 
 // ===== handlClick ===== //
 function handleClick(event) {
+
+  //계산값 변수 설정
+  var calFir = Number(result.innerHTML);
+  var calSec = Number(inputValue.value);
+
   switch (event.target.id) {
     // === Undo === //
     case 'undoButton':
+
+      //바라보는 위치 변경
+      cnt--;
+
       break;
 
     // === Add === //
     case 'addButton':
+    
+      //바라보는 위치 변경
+      cnt++;
+
+      //배열에 해당 값 저장
+      calList[cnt] = calFir + calSec;
+
       break;
 
     // === Sub === //
     case 'subButton':
+
+      //바라보는 위치 변경
+      cnt++;
+
+      //배열에 해당 값 저장
+      calList[cnt] = calFir - calSec;
+
       break;
 
     // === Redo === //
     case 'redoButton':
+
+      //바라보는 위치 변경
+      cnt++;
+
       break;
 
     default:
       return;
   } // switch
+
+  //결과값 세팅
+  result.innerHTML = calList[cnt];
+  if(cnt == -1){
+    result.innerHTML = 0;
+  }
+
+  //입력값 초기화
+  inputValue.value = '';
+  inputValue.focus();
+
+  //redo, undo 버튼 활성화 유무 체크
+  if(cnt == calList.length-1){
+    redoButton.disabled = 'disabled';
+  }else{
+    redoButton.disabled = false;
+  }
+  if(cnt == -1){
+    undoButton.disabled = 'disabled';
+  }else{
+    undoButton.disabled = false
+  }
+
 } // func
